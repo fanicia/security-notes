39986c98ef5755a114b3bb60511f233cf91c84f9
fanicia <mortenzc42@gmail.com>
2023-04-06 13:14:08 -0400

    Add scripts folder for smtp enum.

---
+++ notes/payloads-n-scripts/scripts/smtpenum.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import socket
+import sys
+
+if len(sys.argv) != 3:
+        print("Usage: smtpenum.py <username> <target_ip>")
+        sys.exit(0)
+
+# Create a Socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the Server
+ip = sys.argv[2]
+connect = s.connect((ip,25))
+
+# Receive the banner
+banner = s.recv(1024)
+
+print(banner)
+
+# VRFY a user
+user = (sys.argv[1]).encode()
+s.send(b'VRFY ' + user + b'\r\n')
+result = s.recv(1024)
+
+print(result)
+
+# Close the socket
+s.close()
